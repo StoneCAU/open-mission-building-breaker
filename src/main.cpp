f35275cf48f3582d6ae3e21b0bead5a4cecfd4f2
Seokwoo <seokwoo1999@naver.com>
2025-11-06 22:19:26 +0900

    fix(main): 한글 깨짐 오류 해결
    - 콘솔 UTF-8 설정

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
+#include <windows.h>
+#include <iostream>
+#include "game/Game.h"
+
 int main() {
-    // TODO: 기능 개발
+
+    // 콘솔 UTF-8 설정
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    Game game;
+    game.run();
+
     return 0;
 }
