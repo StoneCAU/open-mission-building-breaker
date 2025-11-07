7ea5cd081b0ab370c8189f8b960d9fa73f43b2b2
Seokwoo <seokwoo1999@naver.com>
2025-11-07 13:23:54 +0900

    feat(main): 키보드 입력 커서 안뜨도록 구현

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,13 @@ int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
+    // 입력 커서 숨기기
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(hConsole, &cursorInfo);
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(hConsole, &cursorInfo);
+
     Game game;
     game.run();
 
