8f5e55dc9888735ea3eb00430a9c83f710287265
Seokwoo <seokwoo1999@naver.com>
2025-11-07 19:47:28 +0900

    refactor(GameConfig): 맵 최대 높이 변경

--- src/game/GameConfig.h
+++ src/game/GameConfig.h
@@ -5,7 +5,7 @@ namespace GameConfig {
     inline constexpr int MAP_WIDTH = 50;
     inline constexpr int MAP_MIN_X = 0;
     inline constexpr int MAP_MAX_X = MAP_WIDTH - 1;
-    inline constexpr int MAP_GROUND_Y = 10;
+    inline constexpr int MAP_GROUND_Y = 20;
 
     // ===== 빌딩 관련 =====
     inline constexpr int BUILDING_WIDTH = 3;
