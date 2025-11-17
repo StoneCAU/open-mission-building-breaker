16d5a66a4e617edf64dface5e3345f6bfcdb7b33
Seokwoo <seokwoo1999@naver.com>
2025-11-18 03:33:17 +0900

    refactor(UIMessage): 필살기 파괴 메세지 수정

--- src/platform/console/UIMessage.h
+++ src/platform/console/UIMessage.h
@@ -28,7 +28,7 @@ private:
     static constexpr const char* DEFENSE_SUCCESS_MSG = "[방어 성공!] 건물을 막아냈습니다!";
     static constexpr const char* PLAYER_DAMAGED_MSG = "[충돌!] 목숨 -1, 콤보 초기화!";
     static constexpr const char* ULTIMATE_PREFIX = "[필살기!] ";
-    static constexpr const char* ULTIMATE_SUFFIX = "개 빌딩 파괴!";
+    static constexpr const char* ULTIMATE_SUFFIX = "개 건물 파괴!";
 
     using Formatter = std::function<std::string(int)>;
     static const std::unordered_map<MessageType, Formatter> formatters;
